86ce3b825153e50bee5c79f93b889abad552728c
Agentpechenka <126567186+Agentpechenka@users.noreply.github.com>
2024-12-19 01:05:05 +0700

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('download').addEventListener('click', function() {
-    const element = document.querySelector('.resume-container');
-    html2pdf()
-        .from(element)
-        .save('резюме.pdf');
-});
